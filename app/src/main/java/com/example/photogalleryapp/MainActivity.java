bfd365d22e1c36c893c831d9e64289f5203cc4e5
Ivan Komkov <jkom2408@gmail.com>
2019-04-30 19:55:35 -0700

    1.5 - Camera Activity - Added simple functionality to snap photos

--- app/src/main/java/com/example/photogalleryapp/MainActivity.java
+++ app/src/main/java/com/example/photogalleryapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.photogalleryapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,26 @@ public class MainActivity extends AppCompatActivity {
     public void launchSearch(View v) {
         Intent i = new Intent(this, SearchActivity.class);
         startActivity(i);
+    }
+
+    public void snapPhoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView imageView = findViewById(R.id.main_imageView);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (data != null && data.getExtras() != null) {
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(imageBitmap);
+            }
+        }
     }
 }
